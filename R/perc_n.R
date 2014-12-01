5dfb5bb75b9a2d9ef33e1d0e969e67f1d3f5906b
raffdoc <raffdoc@yahoo.com>
2014-12-01 13:47:56 +0100

    Two new fuctions added and tested. Functional in simople situations.

---
+++ R/perc_n.R
@@ -0,0 +1,12 @@
+#' Calculate percentage in a numeric vector
+#' @param x is a numeric vector
+#' @param na.rm is remouval of NA values and set defaoul to TRUE
+#' @return numeric value expressed in percents
+#' @export
+perc_n <- function(x,na.rm=TRUE,ref=1,...){
+        require(mosaic)
+        if (na.rm) 
+                x <- x[!is.na(x)]
+        x <- relevel(as.factor(x),ref=ref)
+        return(paste0(perc(~x,format = "count")," ","(",format(perc(~x)),"%",")")) 
+}
